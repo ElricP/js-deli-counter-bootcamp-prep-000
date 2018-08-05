9c80ab99055e1782bf7c01c289f6931cc390ae6f
Yuan Pan <y39pan@edu.uwaterloo.ca>
2018-08-05 20:47:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(line, name){
 }
 
 function nowServing(line){
-  if(line.length == 0){
+  if(line.length === 0){
     return "There is nobody waiting to be served!";
   }
   var first = line[0];
